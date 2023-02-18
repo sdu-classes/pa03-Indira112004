3c642cae15df02842fdfd310e7211dd4fca4ba34
Indira112004 <120215040+Indira112004@users.noreply.github.com>
2023-02-18 23:42:48 +0600

    second methot

--- problem-A/Calculator.java
+++ problem-A/Calculator.java
@@ -42,3 +42,36 @@ public class Calculator {
     }
 
 }
+//second methot
+//public class Calculator {
+//    private int a;
+//    private int b;
+//    public Calculator(int a, int b){
+//        this.a = a;
+//        this.b = b;
+//    }
+//    public int Add()throws ArithmeticException{
+//        if(this.a <0 || this.b <0 ){
+//            throw new ArithmeticException("One of the numbers is negative");
+//        }
+//        return a +b;
+//    }
+//    public int Substract() throws ArithmeticException{
+//        if(this.a < 0 || this.b < 0 ){
+//            throw new ArithmeticException("One of the numbers is negative");
+//        }
+//        return a - b ;
+//    }
+//    public int Multiplication() throws ArithmeticException{
+//        if(this.a == 0 || this.b==0){
+//            throw new ArithmeticException("One of the numbers equal zero");
+//        }
+//        return a * b;
+//    }
+//    public int Division() throws ArithmeticException{
+//        if(this.a ==0 || this.b == 0){
+//            throw new  ArithmeticException("One of the numbers equal zero");
+//        }
+//        return a/b;
+//    }
+//}
