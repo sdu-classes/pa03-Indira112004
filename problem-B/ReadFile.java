2cb771096aeef3733604313fdb2cd0cb8e13d224
Indira112004 <120215040+Indira112004@users.noreply.github.com>
2023-02-18 22:48:25 +0600

    add second methot

--- problem-B/ReadFile.java
+++ problem-B/ReadFile.java
@@ -16,6 +16,7 @@ public class ReadFile {
         }
 
         return lines;
+        
     }
     public static void main(String[] args) {
         try {
@@ -31,3 +32,31 @@ public class ReadFile {
         }
     }
 }
+// second methot
+//public class ReadFile {
+//    public static void readFile(String filename) throws FileNotFoundException {
+//        File file = new File(filename);
+//        Scanner fileIn = new Scanner(file);
+//
+//        String[] lines = new String[1000];
+//        int lineCounter = 0;
+//
+//        while (fileIn.hasNextLine()) {
+//            String line = fileIn.nextLine();
+//            lines[lineCounter] = line;
+//            lineCounter++;
+//        }
+//        for (int i = 0; i < lineCounter; i++) {
+//            System.out.println("["+i+"]"+lines[i]);
+//        }
+//    }
+//    public static void main(String[] args) {
+//        try {
+//             readFile("input.txt");
+//
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("The file does not exist");
+//        }
+//    }
+//}
+
