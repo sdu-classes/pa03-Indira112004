8c8db360dcef2d2613eccd921c1a1f25ab015a22
Indira112004 <120215040+Indira112004@users.noreply.github.com>
2023-02-18 23:41:08 +0600

    Update Main.java

--- problem-A/Main.java
+++ problem-A/Main.java
@@ -32,3 +32,27 @@ public class Main {
 
     }
 }
+// second methot 
+//public class Main {
+//    public static void main(String[] args) throws NumberFormatException{
+//        Scanner scan  = new Scanner(System.in);
+//        try{
+//            int a = scan.nextInt();
+//            int b = scan.nextInt();
+//            Calculator obj1 = new Calculator(a, b);
+//            Calculator obj2 = new Calculator(a, b);
+//            Calculator obj3 = new Calculator(a, b);
+//            Calculator obj4 = new Calculator(a, b);
+//            System.out.println(obj1.Add());
+//            System.out.println( obj2.Substract());
+//            System.out.println( obj3.Multiplication());
+//            System.out.println(obj4.Division());
+//        }
+//        catch(NumberFormatException n){
+//            System.out.println("Your input is non-integer");
+//        }
+//
+//    }
+//}
+
+
